8382277304168912aa1c41df7adb08da980a27f0
yuanbq <2948222214@qq.com>
2021-05-21 10:05:34 +0800

    第一次修改

--- ssm_service/src/test/java/com/ck/AppTest.java
+++ ssm_service/src/test/java/com/ck/AppTest.java
@@ -3,6 +3,7 @@ package com.ck;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import sun.applet.Main;
 
 /**
  * Unit test for simple App.
@@ -16,5 +17,9 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1122221");
     }
 }
